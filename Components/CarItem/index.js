b66ea2beba4a286be8211f992411793fbba527b8
johirjohnny <johirjohnny96@gmail.com>
2021-06-03 22:37:15 +0600

    car item component added

---
+++ Components/CarItem/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { View, Text, ImageBackground } from 'react-native';
+//import StyledButton from "../StyledButton";
+import styles from './styles';
+
+const CarItem = (props) => {
+
+    //const { name, tagline, taglineCTA, image } = props.car;
+
+    return (
+        <View style={styles.carContainer}>
+            <ImageBackground
+                source={require('../../assets/images/ModelX.jpeg')}
+                style={styles.image}
+            />
+
+            <View style={styles.titles}>
+                <Text style={styles.title}>Model S</Text>
+                <Text style={styles.subtitle}>Starting at $69.420</Text>
+
+            </View>
+
+
+        </View>
+
+    );
+};
+
+export default CarItem;
